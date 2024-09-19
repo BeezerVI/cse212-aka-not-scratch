ce92b78f71e1c66765d3aa119acb299bab118bbe
BeezerVI <hyrummckay@gmail.com>
2024-09-19 18:49:05 -0500

    finshed coding project realearning C# forgot how to do all of that

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,24 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    // we are given two values: number and length
+    // to start off, we would like to make code that begins with the first number
+    // for example, if we are given (7, 5), then we want to make a loop that lasts 5 steps
+    // each step, we add an item to a new list, probably called multiples
+    // step one: add an item to the list; the item should be the number * whatever step we are at in the loop
+    // for example, if our loop is about to end at step 5, then the number should be 35, but at step one, it would be 7
+    // this should give us a list of elements that work.
 
-        return []; // replace this return statement with your own
+        // start out by making array
+        double[] multiples = new double[length];
+
+        // make loop
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples;
     }
 
     /// <summary>
@@ -25,9 +37,21 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // we are given a list of numbers and a number to determine how many times to rotate
+        // next, loop for the specified amount of times (amount)
+        // in each loop iteration, remove the last element from the list and insert it at the front
+        // after completing all the rotations, the list should be in the correct order
+
+        for (int i = 0; i < amount; i++)
+        {
+            // Store the last element of the list
+            int lastElement = data[data.Count - 1];
+
+            // Remove the last element
+            data.RemoveAt(data.Count - 1);
+
+            // Insert the stored last element at the beginning of the list
+            data.Insert(0, lastElement);
+        }
     }
 }
